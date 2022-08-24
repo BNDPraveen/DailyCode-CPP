cc15e345b0f1b8a768192d1d5aaa0f7691cf27be
BNDPraveen <praveenbabi124@gmail.com>
2022-08-24 23:29:05 +0530

    least possible time

--- bubblesortSwapCount2.cpp
+++ bubblesortSwapCount2.cpp
@@ -1,3 +1,12 @@
+/* In Bubble sort, largest element moves to right. So a swapping is done, when a smaller element is found on right side.
+So to count number of swaps for an element, just count number of elements on right side which are smaller than it.
+Array is [8, 22, 7, 9, 31, 19, 5, 13].
+For 8, number of elements on right side which are smaller : 2 (7 and 5)
+For 22 : 5 (7,9,19,5,13)
+For 7 : 1, for 9 : 1, for 31 : 3, for 19 : 2, for 5 : 0, for 13 : 0
+Adding all these, we get : 2+5+1+1+3+2 = 14
+So 14 swaps will be done.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
